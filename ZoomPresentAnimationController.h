8d55d280966e08ef4bfdc301f846416294575464
Gina Mullins <littlepeculiar13@yahoo.com>
2013-10-22 18:20:44 -0700

    Create ZoomPresentAnimationController.h

---
+++ ZoomPresentAnimationController.h
@@ -0,0 +1,15 @@
+//
+//  ZoomPresentAnimationController.h
+//  LAFitness
+//
+//  Created by Gina Mullins on 10/15/13.
+//  Copyright (c) 2013 Fitness International LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZoomPresentAnimationController : NSObject <UIViewControllerAnimatedTransitioning>
+
+@property (nonatomic, assign) NSInteger cellSelected;
+
+@end
