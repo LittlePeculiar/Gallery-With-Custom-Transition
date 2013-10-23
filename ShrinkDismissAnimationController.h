9f2032eedf17f706da163c3e75f760d3d656099e
Gina Mullins <littlepeculiar13@yahoo.com>
2013-10-22 18:22:58 -0700

    Create ShrinkDismissAnimationController.h

---
+++ ShrinkDismissAnimationController.h
@@ -0,0 +1,13 @@
+//
+//  ShrinkDismissAnimationController.h
+//  LAFitness
+//
+//  Created by Gina Mullins on 10/15/13.
+//  Copyright (c) 2013 xxxxxxxxxx LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShrinkDismissAnimationController : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
