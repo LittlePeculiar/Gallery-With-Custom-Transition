da9d1ef598534b68e2a73f02519517497f8e496e
Gina Mullins <littlepeculiar13@yahoo.com>
2013-11-07 17:05:30 -0800

    Update ZoomPresentAnimationController.m

--- ZoomPresentAnimationController.m
+++ ZoomPresentAnimationController.m
@@ -3,7 +3,7 @@
 //  LAFitness
 //
 //  Created by Gina Mullins on 10/15/13.
-//  Copyright (c) 2013 xxxxxxxxxx LLC. All rights reserved.
+//  Copyright (c) 2013 Fitness International LLC. All rights reserved.
 //
 
 #import "ZoomPresentAnimationController.h"
@@ -13,14 +13,14 @@
 // specifies the length of the transition animation
 - (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext
 {
-    return 0.3;
+    return 0.5;
 }
 
 - (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext
 {
     // determine the new position for the view to pop from
-    float posX = [self positionX];
-    float posY = [self positionY];
+    float posX = self.touchPoint.x+50;
+    float posY = self.touchPoint.y+50;
     
     //UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     UIViewController *toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
@@ -57,69 +57,5 @@
                               }];
 }
 
-- (float)positionX
-{
-    float posX = 0;
-    int max = 13;   // we currently have 13 pics
-    
-    // easy ones
-    if (self.cellSelected == 0)
-        return 50;
-    else if (self.cellSelected == 1)
-        return  160;
-    else if (self.cellSelected == 2)
-        return 270;
-    else
-    {
-        // determine if cell is on the left
-        for (int i = 3; i < max; i+=3)
-        {
-            if (i == self.cellSelected)
-            {
-                posX = 50;
-                break;
-            }
-        }
-        // determine if cell is in the middle
-        for (int i = 4; i < max; i+=3)
-        {
-            if (i == self.cellSelected)
-            {
-                posX = 160;
-                break;
-            }
-        }
-        // determine if cell is on the right
-        for (int i = 5; i < max; i+=3)
-        {
-            if (i == self.cellSelected)
-            {
-                posX = 270;
-                break;
-            }
-        }
-    }
-    
-    return posX;
-}
-
-- (float)positionY
-{
-    float posY = 0;
-    
-    // no need to be exact
-    if (self.cellSelected <= 2)
-        posY = 100;
-    else if (self.cellSelected > 2 && self.cellSelected <= 5)
-        posY = 250;
-    else if (self.cellSelected > 5 && self.cellSelected <= 8)
-        posY = 350;
-    else if (self.cellSelected > 8 && self.cellSelected <= 11)
-        posY = 450;
-    else
-        posY = 500;
-    
-    return posY;
-}
 
 @end
